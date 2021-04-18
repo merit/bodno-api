aaf042013f9b0264e7aa95092ac91cd424bbd9d1
Jay Schwerberg <jay@gomerits.com>
2021-04-18 12:18:02 -0700

    Create pydantic models

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+class BadgeBase(BaseModel):
+    site: str
+    first_name: str
+    last_name: str
+    email: str
+    organization: str
+    qr_code: str
+    category: Optional[str] = None
+    background: Optional[str] = None
+    headshot_id: Optional[str] = None
+
+class BadgeCreate(BadgeBase):
+    pass
+
+class Badge(BadgeBase):
+    id: int
+
+    class Config:
+        orm_mode = True
